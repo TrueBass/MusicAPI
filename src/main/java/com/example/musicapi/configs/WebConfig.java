926e65e5974860e0a201d213928d4a7d2ce21c62
TrueBass <ttrrbb81@gmail.com>
2025-04-12 23:29:20 +0200

    Defined folder structure. Changed names for some folders from singular to plural form

--- src/main/java/com/example/musicapi/configs/WebConfig.java
+++ src/main/java/com/example/musicapi/configs/WebConfig.java
@@ -1,8 +1,7 @@
-package com.example.musicapi.config;
+package com.example.musicapi.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
--- src/main/java/com/example/musicapi/entities/User.java
+++ src/main/java/com/example/musicapi/entities/User.java
@@ -1,4 +1,4 @@
-package com.example.musicapi.entity;
+package com.example.musicapi.entities;
 
 import jakarta.persistence.*;
 import lombok.*;

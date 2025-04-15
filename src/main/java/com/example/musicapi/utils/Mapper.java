8d4d0d7ba636cb13abadaca80c4b0c37c1119028
markver <vereshchaginmark2022@gmail.com>
2025-04-15 14:30:01 +0200

    Created Mapper class
    - Implemented Mapper class that will be used for basic conversion between entities and dtos via static methods.

---
+++ src/main/java/com/example/musicapi/utils/Mapper.java
@@ -0,0 +1,57 @@
+package com.example.musicapi.utils;
+
+import com.example.musicapi.dtos.playlist_dtos.CreatePlaylistDto;
+import com.example.musicapi.dtos.playlist_dtos.UpdatePlaylistDto;
+import com.example.musicapi.dtos.song_dtos.CreateSongDto;
+import com.example.musicapi.dtos.playlist_dtos.PlaylistDto;
+import com.example.musicapi.dtos.song_dtos.SongDto;
+import com.example.musicapi.entities.Playlist;
+import com.example.musicapi.entities.Song;
+
+public final class Mapper {
+    public static Playlist MapToPlaylist(CreatePlaylistDto dto) {
+        return Playlist.builder()
+                .title(dto.title())
+                .isPrivate(dto.isPrivate())
+                .build();
+    }
+
+    public static Playlist MapToPlaylistDto(UpdatePlaylistDto dto) {
+        return Playlist.builder()
+                .id(dto.id())
+                .title(dto.title())
+                .isPrivate(dto.isPrivate())
+                .build();
+    }
+
+    public static PlaylistDto MapToPlaylistDto(Playlist playlist) {
+        return new PlaylistDto(
+                playlist.getId(),
+                playlist.getTitle(),
+                playlist.getCreatedAt(),
+                playlist.getModifiedAt(),
+                playlist.isPrivate(),
+                playlist.getUser().getId());
+    }
+
+    public static Song MapToSong(CreateSongDto dto) {
+        return Song.builder()
+                .title(dto.title())
+                .author(dto.author())
+                .duration(dto.duration())
+                .data(dto.data())
+                .build();
+    }
+
+    public static SongDto MapToSongDto(Song song) {
+        return new SongDto(
+                song.getId(),
+                song.getTitle(),
+                song.getAuthor(),
+                song.getAddedAt(),
+                song.getLikes(),
+                song.getData(),
+                song.getDuration(),
+                song.getPlaylist().getId());
+    }
+}

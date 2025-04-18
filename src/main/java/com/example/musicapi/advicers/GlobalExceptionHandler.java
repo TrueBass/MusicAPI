41c61d974e0f061fdc713c53d27858f9c0aa3a83
TrueBass <ttrrbb81@gmail.com>
2025-04-18 13:36:26 +0200

    Created rest controller advice for global errors handling. Added AlreadyExistsException handler only. Tested. It's Working fine.

---
+++ src/main/java/com/example/musicapi/advicers/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.musicapi.advicers;
+
+import com.example.musicapi.exceptions.AlreadyExistsException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(AlreadyExistsException.class)
+    public ResponseEntity<String> handleAlreadyExistsException(AlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+}
